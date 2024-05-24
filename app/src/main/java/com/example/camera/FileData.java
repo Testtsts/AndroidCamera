43dec01182ee015ef268e352a96c8cf91de090f7
Testtsts <ihsannur0610@gmail.com>
2024-05-24 21:50:48 +0700

    fileData class

---
+++ app/src/main/java/com/example/camera/FileData.java
@@ -0,0 +1,37 @@
+package com.example.camera;
+
+public class FileData {
+    private int id;
+    private String fileName;
+    private String path;
+
+    public FileData(int id, String fileName, String path) {
+        this.id = id;
+        this.fileName = fileName;
+        this.path = path;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
