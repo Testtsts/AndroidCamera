7024248b2edf0fdb5b1334d86f975f10ebf938d1
Testtsts <ihsannur0610@gmail.com>
2024-07-01 10:09:38 +0700

    change api item file to match be changes

--- app/src/main/java/com/example/camera/FileData.java
+++ app/src/main/java/com/example/camera/FileData.java
@@ -1,14 +1,29 @@
 package com.example.camera;
 
+import android.util.Log;
+
+import org.json.JSONObject;
+
 public class FileData {
     private int id;
     private String fileName;
     private String fileData;
 
+    private String extractBase64(String JSONString){
+        try {
+            JSONObject obj = new JSONObject(JSONString);
+            return obj.getString("base64");
+        }
+        catch (Throwable t){
+            Log.e("err","error parsing");
+            return null;
+        }
+
+    }
     public FileData(int id, String fileName, String fileData) {
         this.id = id;
         this.fileName = fileName;
-        this.fileData = fileData;
+        this.fileData = extractBase64(fileData);
     }
 
 
